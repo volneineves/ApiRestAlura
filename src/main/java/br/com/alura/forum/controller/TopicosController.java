bf96bcf8a43afb7431913ba8b19f07c9309e7884
volneineves <_S9RFz_YPe!vXwb>
2021-11-12 22:25:33 -0300

    Add TopicoRepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,25 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     @ResponseBody
     public List<TopicoDto> lista(){
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topico = topicoRepository.findAll();
+        return TopicoDto.converter(topico);
     }
 }
--- src/main/java/br/com/alura/forum/modelo/Curso.java
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -13,11 +13,6 @@ public class Curso {
 	private String nome;
 	private String categoria;
 
-	public Curso(String nome, String categoria) {
-		this.nome = nome;
-		this.categoria = categoria;
-	}
-
 	public Curso() {
 
 	}
