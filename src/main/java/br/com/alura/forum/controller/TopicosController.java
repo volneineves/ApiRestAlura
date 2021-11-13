226bcc686012ef8f68d0c5a3de7213d1c10d833b
volneineves <_S9RFz_YPe!vXwb>
2021-11-12 21:14:10 -0300

    Add DTOS

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,22 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
     @ResponseBody
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
         Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 }
