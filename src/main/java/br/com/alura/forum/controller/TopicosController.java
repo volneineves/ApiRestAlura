766129019503baee4d91e815f2b04e95b82ec32a
volneineves <_S9RFz_YPe!vXwb>
2021-11-14 20:40:31 -0300

    Insert Pageable

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,14 +3,15 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.dto.DetalhesTopicoDto;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.form.AtualizacaoTopicoForm;
+import br.com.alura.forum.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
-import br.com.alura.forum.form.TopicoForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -30,16 +31,17 @@ public class TopicosController {
     @Autowired
     private CursoRepository cursoRepository;
 
+//    @GetMapping
+//    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordernacao){ // Esse parâmetro deve ser passado igual ao nome da variável:http://localhost:8080/topicos?nomeCurso=Spring+Boot
+//        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordernacao);// O Sort.Direction.ASC pega o valor da ordenação para parâmetro de crescente
 
     @GetMapping
-    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd){ // Esse parâmetro deve ser passado igual ao nome da variável:http://localhost:8080/topicos?nomeCurso=Spring+Boot
-
-        Pageable paginacao = PageRequest.of(pagina, qtd);
-
-        if(nomeCurso == null){
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 0) Pageable paginacao) { //Com a anotação @EnableSpringDataWebSupport na classe main é possível receber os parâmetros.Obs.: Agora os parâmetros são passados em inglês pois não criamos variáveis no nome em português
+                                                                                                         //http://localhost:8080/topicos?page=0&size=10&sort=titulo,desc
+        if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
-        }else{
+        } else {
             Page<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso, paginacao);
             return TopicoDto.converter(topicos);
         }
@@ -47,7 +49,7 @@ public class TopicosController {
 
     @PostMapping
     @Transactional // Avisar ao Spring pra comitar ao final do método
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
@@ -57,11 +59,11 @@ public class TopicosController {
     //Parâmetro dinâmico
     //Variável do Path, da url
     @GetMapping("/{id}")
-    public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id){
+    public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
 
         Optional<Topico> topico = topicoRepository.findById(id); // findById retorna um Optional e não um Exception
 
-        if(topico.isPresent()){
+        if (topico.isPresent()) {
             return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
         }
         return ResponseEntity.notFound().build();
@@ -69,10 +71,10 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional // Avisar ao Spring pra comitar ao final do método
-    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
 
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             Topico topico = form.atualizar(id, topicoRepository);
             return ResponseEntity.ok(new TopicoDto(topico));
         }
@@ -82,10 +84,10 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional // Avisar ao Spring pra comitar ao final do método
-    public ResponseEntity remover(@PathVariable Long id){
+    public ResponseEntity remover(@PathVariable Long id) {
         Optional<Topico> optional = topicoRepository.findById(id);
 
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             topicoRepository.deleteById(id);
             return ResponseEntity.ok().build();
         }
