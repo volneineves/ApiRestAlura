47481d29ea578b7a5e6f6e197bc0b5d4fd6b6f97
volneineves <_S9RFz_YPe!vXwb>
2021-11-14 21:07:02 -0300

    Insert @CacheEvict

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -52,6 +53,7 @@ public class TopicosController {
 
     @PostMapping
     @Transactional // Avisar ao Spring pra comitar ao final do método
+    @CacheEvict(value = "listaDeTopicos", allEntries = true) // O value do @CacheEvict deve ser igual ao cache que deseja atualizar, no caso, do método "lista"
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -74,6 +76,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional // Avisar ao Spring pra comitar ao final do método
+    @CacheEvict(value = "listaDeTopicos", allEntries = true) // O value do @CacheEvict deve ser igual ao cache que deseja atualizar, no caso, do método "lista"
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
 
@@ -87,6 +90,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional // Avisar ao Spring pra comitar ao final do método
+    @CacheEvict(value = "listaDeTopicos", allEntries = true) // O value do @CacheEvict deve ser igual ao cache que deseja atualizar, no caso, do método "lista"
     public ResponseEntity remover(@PathVariable Long id) {
         Optional<Topico> optional = topicoRepository.findById(id);
 
