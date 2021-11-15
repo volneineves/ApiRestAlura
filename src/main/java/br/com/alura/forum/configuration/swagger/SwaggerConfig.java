a84df473e502cd3c975aefa9ebf0a322ef4ee353
volneineves <_S9RFz_YPe!vXwb>
2021-11-15 18:49:03 -0300

    Implementação do Swagger com JWT.

--- src/main/java/br/com/alura/forum/configuration/swagger/SwaggerConfig.java
+++ src/main/java/br/com/alura/forum/configuration/swagger/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package br.com.alura.forum.configuration.swagger;
 
+import br.com.alura.forum.modelo.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -15,8 +20,19 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                        .name("Authorization")
+                                        .description("Header para Token JWT")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build()));
+
     }
 }
